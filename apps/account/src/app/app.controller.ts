69edab7a8bae530d2b5e1ab3b47fa792232ec126
Anton Kuchmasov <anton.kuchmasov.dev@gmail.com>
2024-06-24 14:09:40 +0300

    added models for auth

--- apps/account/src/app/app.controller.ts
+++ apps/account/src/app/app.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
+import {} from '@school/interfaces';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
